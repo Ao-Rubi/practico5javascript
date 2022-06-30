7dc2f150b5ed0e8469480ea6e683ffcbc457aed4
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-06-29 21:38:42 -0300

    trabado en la logica del contador

--- js/ejercicios/ejercicio-6-tp5.js
+++ js/ejercicios/ejercicio-6-tp5.js
@@ -1,3 +1,4 @@
 let selectHora = document.getElementById("hora").value;
 let selectMinuto = document.getElementById("minuto").value;
 let selectSegundo = document.getElementById("segundo").value;
+let contador = document.getElementById("timer");
